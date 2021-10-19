4c1a922325a53c15f09e42d6026a54de429b8cea
Gustavo Stingelin Cardoso <gustavo.stingelin.c@outlook.com>
2021-10-18 23:24:00 -0300

    add UNAUTHORIZED to anonymous /api request

--- src/main/java/com/gs/pi4/api/config/SecurityConfig.java
+++ src/main/java/com/gs/pi4/api/config/SecurityConfig.java
@@ -6,11 +6,13 @@ import com.gs.pi4.api.api.security.jwt.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -36,11 +38,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .csrf().disable()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
+                .requiresChannel(channel -> 
+                    channel.anyRequest().requiresSecure())
                 .authorizeRequests()
                 .antMatchers("/auth/signin", "/auth/signup", "/api-docs/**", "/swagger-ui.html**").permitAll()
                 .antMatchers("/api/**").authenticated()
                 .antMatchers("/users").denyAll()
             .and()
+                .antMatcher("/api/**").anonymous().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+
+            .and()
                 .apply(new JwtConfigurer(tokenProvider));
     }
+
 }
